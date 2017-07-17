ec4ae1b8752d372f60ecbb386e9828cf25f63eae
Laggg <ddude88888@gmail.com>
2017-07-16 23:53:03 -0400

    Initial commit

---
+++ src/FormatResourceTxt.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FormatResourceTxt {
+	public static void main(String[] args) {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\ddude\\Desktop\\forge obfuscation.txt"));
+			String line = in.readLine();
+			ArrayList<String> list = new ArrayList<String>();
+			while(line!=null) {
+				Pattern p = Pattern.compile("(.*_.*_.*)\\s+(.*)");
+				Matcher m = p.matcher(line);
+				m.matches();
+				list.add(m.group(1)+":"+m.group(2));
+				line = in.readLine();
+			}
+			in.close();
+			BufferedWriter out = new BufferedWriter(new FileWriter("C:\\Users\\ddude\\Desktop\\forge obfuscation2.txt"));
+			for(int i = 0; i<list.size(); i++) {
+				out.write(list.get(i) + "\r\n");
+			}
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
